e039e48fd01aafc50a162de44662f6103b2f944a
Duncan Townsend <git@duncancmt.com>
2024-11-16 07:30:09 -0500

    Remove unused

--- src/interfaces/IERC6093.sol
+++ src/interfaces/IERC6093.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.8.25;
 
 interface IERC6093 {
     error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
-    error ERC20InvalidSender(address sender);
     error ERC20InvalidReceiver(address receiver);
     error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
-    error ERC20InvalidApprover(address approver);
-    error ERC20InvalidSpender(address spender);
 }
