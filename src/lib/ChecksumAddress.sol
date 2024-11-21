a42d2d8d1d844b4027a23658b2653c60c569a1c1
Duncan Townsend <git@duncancmt.com>
2024-11-20 20:46:26 -0500

    Comments

--- src/lib/ChecksumAddress.sol
+++ src/lib/ChecksumAddress.sol
@@ -14,23 +14,36 @@ library ChecksumAddress {
 
             addr := shl(0x60, addr)
             let o := add(0x22, r)
+            // Hexlify `addr` and write it into the output region. This is a do..while loop.
             for { let i } true {} {
                 let p := add(o, add(i, i))
                 let temp := byte(i, addr)
+                // Split `temp` into nibbles and output the corresponding lookup table entries
                 mstore8(add(0x01, p), mload(and(0x0f, temp)))
                 mstore8(p, mload(shr(0x04, temp)))
                 i := add(i, 0x01)
                 if eq(i, 0x14) { break }
             }
-            let mask := 0x4040404040404040404040404040404040404040404040404040404040404040
-            let hash := and(0x8888888888888888888888888888888888888888888888888888888888888880, keccak256(o, 0x28))
+
+            // EIP-55 checksum is based on the keccak of the hexlified address. Hash it and extract
+            // the hash bits that (might) form the checksum.
+            let hash := and(0x8888888888888888888888888888888888888888000000000000000000000000, keccak256(o, 0x28))
+
+            // Again, in a do..while, space the nibble-spaced bits of the hash into byte-spaced and
+            // aligned as bit 6 of each byte
             for { let i } true {} {
                 mstore(add(i, i), mul(0x88000000000000000000000000000000000000000000000000000000000000, byte(i, hash)))
                 i := add(i, 0x01)
                 if eq(i, 0x14) { break }
             }
+
+            let mask := 0x4040404040404040404040404040404040404040404040404040404040404040
+            // Extract bit 6 of each byte of the hexlified output (indicates that the character is
+            // a..f) and mask the hash with this. Shift those bits up to bit 7 (the case bit) and
+            // flip it to zero (uppercase) wherever the hash is set
             mstore(o, xor(mload(o), shr(0x01, and(mload(0x00), and(mload(o), mask)))))
             o := add(o, 0x20)
+            // Do it again for the second word of the hexlified output
             mstore(o, xor(mload(o), shr(0x01, and(mload(0x20), and(mload(o), mask)))))
         }
     }
