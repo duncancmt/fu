c7f445f3863c410d741ac8d25ab00d70b3fff2ba
Duncan Townsend <git@duncancmt.com>
2024-11-25 11:03:36 -0500

    Make `slither` happier

--- src/FU.sol
+++ src/FU.sol
@@ -450,6 +450,7 @@ contract FU is IERC2612, IERC5267, IERC5805, IERC6093, IERC7674, TransientStorag
     }
 
     function clock() public view override returns (uint48) {
+        // slither-disable-next-line divide-before-multiply
         return uint48(block.timestamp / 86400 * 86400);
     }
 
