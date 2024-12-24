b515ebb38a1b3c820b125ce6906bf1b5e4642948
Duncan Townsend <git@duncancmt.com>
2024-12-24 16:53:55 -0500

    Make token only deployable by `duncancmt.eth`

--- src/FU.sol
+++ src/FU.sol
@@ -81,6 +81,7 @@ contract FU is FUStorage, TransientStorageLayout, ERC20Base {
         require(Settings.SHARES_TO_VOTES_DIVISOR >= Settings.INITIAL_SHARES_RATIO);
 
         require(msg.sender == 0x4e59b44847b379578588920cA78FbF26c0B4956C);
+        require(tx.origin == 0xD6B66609E5C05210BE0A690aB3b9788BA97aFa60);
         require(msg.value >= 1 ether);
         require(initialHolders.length >= Settings.ANTI_WHALE_DIVISOR * 2);
 
