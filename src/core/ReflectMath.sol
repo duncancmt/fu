cd8da4ceb3f0ce2f1f00c4f172748c2cd1907ec7
Duncan Townsend <git@duncancmt.com>
2024-12-29 16:40:22 -0500

    Bug! Rounding error

--- src/core/ReflectMath.sol
+++ src/core/ReflectMath.sol
@@ -239,7 +239,7 @@ library ReflectMath {
         // Called when `to`'s final shares will be the whale limit and `from` is sending their entire balance
         newToShares = (totalShares - fromShares - toShares).div(Settings.ANTI_WHALE_DIVISOR - 1) - ONE_SHARE;
         counterfactualToShares = (
-            totalShares - castUp(scale(fromShares, BASIS - taxRate)).mul(Settings.ANTI_WHALE_DIVISOR)
+            totalShares - cast(scale(fromShares, BASIS - taxRate)).mul(Settings.ANTI_WHALE_DIVISOR)
         ).div(Settings.ANTI_WHALE_DIVISOR);
         newTotalShares = totalShares + newToShares - fromShares - toShares;
 
