4a575f98a68b99aa00e746aed30084fc512325d2
Duncan Townsend <git@duncancmt.com>
2025-01-17 22:47:20 -0500

    Revert "Golf"
    This reverts commit 885961eb80c20a04ff13f02eab0d186137842818.

--- src/lib/FastTransferLib.sol
+++ src/lib/FastTransferLib.sol
@@ -13,9 +13,9 @@ library FastTransferLib {
             // in a start of calldata at offset 16. Calldata is 36 bytes long (4 bytes selector, 32
             // bytes argument).
             if iszero(staticcall(gas(), token, 0x10, 0x24, 0x00, 0x20)) {
-                // For contract size, we don't bother to copy the revert reason and bubble it. We
-                // just throw an empty revert reason
-                revert(0x00, 0x00)
+                let ptr := mload(0x40)
+                returndatacopy(ptr, 0x00, returndatasize())
+                revert(ptr, returndatasize())
             }
             // We assume that `token`'s code exists and that it conforms to ERC20 (won't return
             // short calldata). We do not bother to check for either of these conditions.
@@ -36,9 +36,9 @@ library FastTransferLib {
             // Calldata starts at offset 16 and is 68 bytes long (2 * 32 + 4). We're not checking
             // the return value, so we don't bother to copy the returndata into memory.
             if iszero(call(gas(), token, 0x00, 0x10, 0x44, 0x00, 0x00)) {
-                // For contract size, we don't bother to copy the revert reason and bubble it. We
-                // just throw an empty revert reason.
-                revert(0x00, 0x00)
+                let ptr := mload(0x40)
+                returndatacopy(ptr, 0x00, returndatasize())
+                revert(ptr, returndatasize())
             }
             // We assume that the token we're calling is well-behaved. We don't check that it
             // might've returned `false`.
