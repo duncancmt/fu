a1b665b634df45434d112a0e332fd651da0c4ffd
fluffypill0w <77161947+fluffypill0w@users.noreply.github.com>
2025-01-25 18:23:35 +0100

    Mutation boilerplate for foundry, doesn't work

--- test/FU.t.sol
+++ test/FU.t.sol
@@ -21,6 +21,9 @@ contract FUTest is Boilerplate, Test {
 
     IFU internal fu;
     address[] internal actors;
+    mapping(address => bool) internal isActor;
+    mapping(address => uint256) internal lastBalance;
+    mapping(address => address) internal shadowDelegates;
 
     function deployFuDependenciesFoundry() internal {
         // Deploy WETH
@@ -31,6 +34,7 @@ contract FUTest is Boilerplate, Test {
             if xor(weth, create(0x00, add(0x20, initcode), mload(initcode))) { revert(0x00, 0x00) }
         }
         label(weth, "WETH");
+        excludeContract(weth);
 
         // Deploy the UniswapV2 factory
         initcode = univ2FactoryInit;
@@ -40,6 +44,7 @@ contract FUTest is Boilerplate, Test {
             if xor(univ2Factory, create(0x00, add(0x20, initcode), mload(initcode))) { revert(0x00, 0x00) }
         }
         label(univ2Factory, "Uniswap V2 Factory");
+        excludeContract(univ2Factory);
 
         // Optionally deploy the deployment proxy, if it doesn't exist
         if (deterministicDeployerFactory.codehash != 0x2fa86add0aed31f33a762c9d88e807c475bd51d0f52bd0955754b2608f7e4989)
@@ -85,6 +90,7 @@ contract FUTest is Boilerplate, Test {
                 holder := keccak256(0x00, 0x20)
             }
             initialHolders[i] = holder;
+            isActor[holder] = true;
         }
         initialHolders.quickSort();
         actors = initialHolders;
@@ -110,20 +116,125 @@ contract FUTest is Boilerplate, Test {
         require(success);
         fu = IFU(payable(address(uint160(bytes20(returndata)))));
         label(address(fu), "FU");
+        excludeContract(address(fu));
 
         // Lock initial liquidity
         IUniswapV2Pair pair = IUniswapV2Pair(fu.pair());
         label(address(pair), "FU/WETH UniV2 Pair");
         pair.mint(address(0));
+        excludeContract(address(pair));
 
         label(0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD, "Super dead");
+
+        lastBalance[address(pair)] = fu.balanceOf(address(pair));
+        lastBalance[0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD] = fu.balanceOf(0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD);
+        for (uint256 i; i < initialHolders.length; i++) {
+            address initialHolder = initialHolders[i];
+            lastBalance[initialHolder] = fu.balanceOf(initialHolder);
+        }
     }
 
     function setUp() public virtual override {
-        return deployFu();
+        deployFu();
+        targetContract(address(this));
     }
 
-    /*
-    function test_vacuous() external {}
-    */
+    function callOptionalReturn(bytes memory data) internal {
+        (bool success, bytes memory returndata) = address(fu).call(data);
+        if (!success) {
+            assembly ("memory-safe") {
+                revert(add(0x20, returndata), mload(returndata))
+            }
+        }
+        if (returndata.length != 0) {
+            require(returndata.length == 32);
+            success = abi.decode(data, (bool));
+            require(success, "returned `false` to signal failure");
+        } else {
+            require(address(fu).code.length != 0);
+        }
+    }
+
+    function getActor(uint256 actorIndex) internal returns (address actor) {
+        actor = actors[actorIndex % actors.length];
+        uint256 balance = fu.balanceOf(actor);
+        assertGe(balance, lastBalance[actor], "negative rebase");
+        lastBalance[actor] = balance;
+        assertEq(fu.delegates(actor), shadowDelegates[actor]);
+    }
+
+    function maybeCreateActor(address newActor) internal {
+        if (newActor == fu.pair()) {
+            return;
+        }
+        if (newActor == 0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD) {
+            return;
+        }
+        if (isActor[newActor]) {
+            return;
+        }
+        isActor[newActor] = true;
+        actors.push(newActor);
+        assertEq(fu.balanceOf(newActor), 0);
+        assertEq(fu.delegates(newActor), address(0));
+    }
+
+    function saveActor(address actor) internal {
+        if (actor != fu.pair()) {
+            assertNotEq(actor, 0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD);
+            assertTrue(isActor[actor]);
+        }
+        lastBalance[actor] = fu.balanceOf(actor);
+        shadowDelegates[actor] = fu.delegates(actor);
+    }
+
+    function addActor(address newActor) external {
+        assume(newActor != 0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD);
+        maybeCreateActor(newActor);
+        saveActor(newActor);
+    }
+
+    function transfer(uint256 actorIndex, address to, uint256 amount) external {
+        address actor = getActor(actorIndex);
+        maybeCreateActor(to);
+
+        vm.prank(actor);
+        callOptionalReturn(abi.encodeCall(fu.transfer, (to, amount)));
+
+        saveActor(actor);
+        saveActor(to);
+    }
+
+    function delegate(uint256 actorIndex, address delegatee) external {
+        address actor = getActor(actorIndex);
+        maybeCreateActor(delegatee);
+
+        vm.prank(actor);
+        fu.delegate(delegatee); // ERC5805 requires that this function return nothing or revert
+
+        saveActor(actor);
+        saveActor(delegatee);
+    }
+
+    function burn(uint256 actorIndex, uint256 amount) external {
+        address actor = getActor(actorIndex);
+
+        vm.prank(actor);
+        callOptionalReturn(abi.encodeCall(fu.burn, (amount)));
+
+        saveActor(actor);
+    }
+
+    function deliver(uint256 actorIndex, uint256 amount) external {
+        address actor = getActor(actorIndex);
+
+        vm.prank(actor);
+        callOptionalReturn(abi.encodeCall(fu.deliver, (amount)));
+
+        saveActor(actor);
+    }
+
+
+    function test_vacuous() external pure {}
+    function invariant_vacuous() external pure {}
 }
