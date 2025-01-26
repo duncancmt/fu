5674348700b50cbef081b1ee97bc20085cbb1797
Duncan Townsend <git@duncancmt.com>
2025-01-26 10:23:58 -0500

    Update salt

--- test/FU.t.sol
+++ test/FU.t.sol
@@ -601,7 +601,7 @@ contract FUInvariants is StdInvariant, Common, ListOfInvariants {
         setBaseFee(6 wei); // causes the `isSimulation` check to pass; Medusa is unable to prank `tx.origin`
         prank(fuTxOrigin);
         (bool success, bytes memory returndata) = deterministicDeployerFactory.call{value: 5 ether}(
-            bytes.concat(bytes32(0x00000000000000000000000000000000000000000000000000000000260e3f9d), initcode)
+            bytes.concat(bytes32(0x000000000000000000000000000000000000000000000000000000027b1e675f), initcode)
         );
         require(success);
         fu = IFU(payable(address(uint160(bytes20(returndata)))));
