fe60b5a7fa5fa485819cd2d3e0f4a943025e404a
Duncan Townsend <git@duncancmt.com>
2025-02-03 16:41:49 -0500

    Add `Ternary.swap`

--- src/lib/Ternary.sol
+++ src/lib/Ternary.sol
@@ -13,4 +13,12 @@ library Ternary {
             r := xor(y, mul(xor(x, y), c))
         }
     }
+
+    function swap(bool c, uint256 x, uint256 y) internal pure returns (uint256 a, uint256 b) {
+        assembly ("memory-safe") {
+            let t := mul(xor(x, y), c)
+            a := xor(x, t)
+            b := xor(y, t)
+        }
+    }
 }
