8960c50c5a841fb751f00c5e9d981b2d0cfec7c2
Duncan Townsend <git@duncancmt.com>
2025-02-03 15:30:10 -0500

    Add some more functions to `FastUniswapV2PairLib`

--- src/interfaces/IUniswapV2Pair.sol
+++ src/interfaces/IUniswapV2Pair.sol
@@ -4,11 +4,34 @@ pragma solidity ^0.8.28;
 import {IERC2612} from "./IERC2612.sol";
 
 interface IUniswapV2Pair is IERC2612 {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve0, uint32 blockTimestampLast);
+
     function sync() external;
-    function mint(address to) external returns (uint256);
+    function mint(address to) external returns (uint256 liquidity);
+    function burn(address to) external returns (uint256 amount0, uint256 amount1);
 }
 
 library FastUniswapV2PairLib {
+    function fastGetReserves(IUniswapV2Pair) internal view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) {
+        assembly ("memory-safe") {
+            let ptr := mload(0x40)
+
+            mstore(0x00, 0x0902f1ac)
+
+            if iszero(staticcall(gas(), pair, 0x1c, 0x04, 0x00, 0x60)) {
+                let ptr := mload(0x40)
+                returndatacopy(ptr, 0x00, returndatasize())
+                revert(ptr, returndatasize())
+            }
+
+            reserve0 := mload(0x00)
+            reserve1 := mload(0x20)
+            blockTimestampLast := mload(0x40)
+
+            mstore(0x40, ptr)
+        }
+    }
+
     function fastSync(IUniswapV2Pair pair) internal {
         assembly ("memory-safe") {
             mstore(0x00, 0xfff6cae9) // Selector for `sync()`
@@ -23,16 +46,30 @@ library FastUniswapV2PairLib {
         }
     }
 
-    function fastMint(IUniswapV2Pair pair, address to) internal returns (uint256 r) {
+    function fastMint(IUniswapV2Pair pair, address to) internal returns (uint256 liquidity) {
         assembly ("memory-safe") {
             mstore(0x14, to)
-            mstore(0x00, 0x6a627842000000000000000000000000)
+            mstore(0x00, 0x6a627842000000000000000000000000) // selector for `mint(address)` with `to`'s padding
             if iszero(call(gas(), pair, 0x00, 0x10, 0x24, 0x00, 0x20)) {
                 let ptr := mload(0x40)
                 returndatacopy(ptr, 0x00, returndatasize())
                 revert(ptr, returndatasize())
             }
-            r := mload(0x00)
+            liquidity := mload(0x00)
+        }
+    }
+
+    function fastMint(IUniswapV2Pair pair, address to) internal returns (uint256 amount0, uint256 amount1) {
+        assembly ("memory-safe") {
+            mstore(0x14, to)
+            mstore(0x00, 0x89afcb44000000000000000000000000) // selector for `burn(address)` with `to`'s padding
+            if iszero(call(gas(), pair, 0x00, 0x10, 0x24, 0x00, 0x40)) {
+                let ptr := mload(0x40)
+                returndatacopy(ptr, 0x00, returndatasize())
+                revert(ptr, returndatasize())
+            }
+            amount0 := mload(0x00)
+            amount1 := mload(0x20)
         }
     }
 }
