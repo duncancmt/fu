b94b919c2562aeb4a6f17b7a50600721ec5905a2
Duncan Townsend <git@duncancmt.com>
2025-02-04 22:11:42 -0500

    WIP: mine salt for Buyback too

--- mine/src/main.rs
+++ mine/src/main.rs
@@ -33,30 +33,46 @@ fn leading_zeros(addr: Address) -> Option<u32> {
     Some(r)
 }
 
+fn leading_ones(addr: Address) -> Option<u32> {
+    let b = addr.as_slice();
+    let mut r = 0u32;
+
+    for c in b.chunks_exact(4) {
+        let w = u32::from_be_bytes(c.try_into().ok()?);
+        let z = (!w).leading_zeros();
+        if z < 32 {
+            return Some(r + z);
+        }
+        r += 32;
+    }
+    Some(r)
+}
+
 #[repr(C)]
 struct B256Aligned(B256, [usize; 0]);
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <initcode_hash> <leading_zeros>", args[0]);
+    if args.len() != 4 {
+        eprintln!("Usage: {} <token_initcode_hash> <buyback_initcode> <leading_zeros>", args[0]);
         eprintln!("Example:");
         eprintln!(
-            "  {} 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54 16",
+            "  {} 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f 0x... 16",
             args[0]
         );
         process::exit(1);
     }
 
-    let initcode: B256 = hex::FromHex::from_hex(args[1].clone())?;
+    let token_inithash: B256 = hex::FromHex::from_hex(args[1].clone())?;
+    let mut buyback_initcode: Vec<u8> = hex::FromHex::from_hex(args[2].clone())?;
 
-    let target: usize = args[2]
+    let target: usize = args[3]
         .parse()
         .expect("invalid integer for number of leading zero bits");
 
     println!("Required leading zero bits: {target}");
 
-    let mut handles = Vec::with_capacity(N_THREADS);
+    let mut token_handles = Vec::with_capacity(N_THREADS);
     let found = Arc::new(AtomicBool::new(false));
     let timer = Instant::now();
 
@@ -82,7 +98,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
 
                 for _ in 0..BATCH_SIZE {
-                    let token_address = DEPLOYER.create2(&salt.0, &initcode);
+                    let token_address = DEPLOYER.create2(&salt.0, &token_inithash);
 
                     let (token0, token1) = if token_address < WETH {
                         (token_address, WETH)
@@ -109,22 +125,77 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         });
 
-        handles.push(handle);
+        token_handles.push(handle);
     }
 
-    let results = handles
+    let results_token = token_handles
         .into_iter()
         .filter_map(|h| h.join().unwrap())
         .collect::<Vec<_>>();
-    let (salt, token_address, pair_address) = results.into_iter().min().unwrap();
+    let (token_salt, token_address, pair_address) = results_token.into_iter().min().unwrap();
+
+    println!("Found token salt {token_salt}. Mining buyback salt...");
+
+    found.store(false, Ordering::SeqCst);
+    let mut buyback_handles = Vec::with_capacity(N_THREADS);
+    let mut token_address_abiencoded = [0u8; 32];
+    token_address_abiencoded[12..].copy_from_slice(token_address.as_slice());
+    buyback_initcode.extend(token_address_abiencoded);
+    let buyback_inithash: B256 = keccak256(buyback_initcode);
+
+    for thread_idx in 0..N_THREADS {
+        let found = Arc::clone(&found);
+
+        let handle = thread::spawn(move || {
+            let mut salt = B256Aligned(B256::ZERO, []);
+            // SAFETY: B256 is aligned enough to treat the last 8 bytes as a `u64`.
+            let salt_word = unsafe {
+                &mut *salt
+                    .0
+                    .as_mut_ptr()
+                    .add(32 - std::mem::size_of::<u64>())
+                    .cast::<u64>()
+            };
+            *salt_word = (thread_idx as u64).to_be();
+
+            'outer: loop {
+                if found.load(Ordering::Relaxed) {
+                    break None;
+                }
+
+                for _ in 0..BATCH_SIZE {
+                    let buyback_address = DEPLOYER.create2(&salt.0, &buyback_inithash);
+
+                    if leading_ones(buyback_address)? as usize > target {
+                        found.store(true, Ordering::Relaxed);
+                        break 'outer Some((salt.0, buyback_address));
+                    }
+
+                    *salt_word = u64::from_be(*salt_word)
+                        .wrapping_add(N_THREADS as u64)
+                        .to_be();
+                }
+            }
+        });
+
+        buyback_handles.push(handle);
+    }
+
+    let results_buyback = buyback_handles
+        .into_iter()
+        .filter_map(|h| h.join().unwrap())
+        .collect::<Vec<_>>();
+    let (buyback_salt, buyback_address) = results_buyback.into_iter().min().unwrap();
 
     println!(
         "Successfully found contract address in {:?}",
         timer.elapsed()
     );
-    println!("Salt:          {salt}");
-    println!("Token Address: {token_address}");
-    println!("Pair Address:  {pair_address}");
+    println!("Token Salt:      {token_salt}");
+    println!("Buyback Salt:    {buyback_salt}");
+    println!("Token Address:   {token_address}");
+    println!("Pair Address:    {pair_address}");
+    println!("Buyback Address: {buyback_address}");
 
     Ok(())
 }
