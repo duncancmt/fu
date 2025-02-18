e95d96ac2251ec746047491857c41714bad55ccc
Duncan Townsend <git@duncancmt.com>
2025-02-18 13:28:25 -0500

    Too many parens; it was bothering me

--- test/FUApprovals.t.sol
+++ test/FUApprovals.t.sol
@@ -23,7 +23,7 @@ contract FUApprovalsTest is FUDeploy, Test {
             maybeCreateActor(spender);
         }
 
-        uint256 beforeAllowance = uint256(vm.load(address(fu), (keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))))); //allowance mapping offset is 8, see https://github.com/duncancmt/fu/blob/c64c7b7fbafd1ea362c056e4fecef44ed4ac5688/src/FUStorage.sol#L16-L26
+        uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8)))))); //allowance mapping offset is 8, see https://github.com/duncancmt/fu/blob/c64c7b7fbafd1ea362c056e4fecef44ed4ac5688/src/FUStorage.sol#L16-L26
 
         vm.recordLogs();
         vm.startStateDiffRecording();
@@ -34,7 +34,7 @@ contract FUApprovalsTest is FUDeploy, Test {
         VmSafe.AccountAccess[] memory accountAccesses = vm.stopAndReturnStateDiff();
         VmSafe.Log[] memory logs = vm.getRecordedLogs();
 
-        uint256 afterAllowance = uint256(vm.load(address(fu), (keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8)))))));
+        uint256 afterAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
 
         if (spender == PERMIT2) {
             assertEq(beforeAllowance, afterAllowance, "permit2 allowance should already be maximum");
