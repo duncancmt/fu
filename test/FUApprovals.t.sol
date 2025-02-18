3c32be7a6d926d86eca03287c98c26865f168345
fluffypill0w <77161947+fluffypill0w@users.noreply.github.com>
2025-02-18 22:08:21 +0100

    smol bug? approving pair address as spender

--- test/FUApprovals.t.sol
+++ test/FUApprovals.t.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.28;
 
 import {FUDeploy, Common} from "./Deploy.t.sol";
+import {ERC20Base} from "../src/core/ERC20Base.sol";
 
 import {Test} from "@forge-std/Test.sol";
 import {VmSafe} from "@forge-std/Vm.sol";
 import {StdCheats} from "@forge-std/StdCheats.sol";
 
 import {console} from "@forge-std/console.sol";
+
+address constant PERMIT2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+address constant pair = 0x00000000e2Ed5120a2fAc7532764DC11F5Ee8fDd;
 
 contract FUApprovalsTest is FUDeploy, Test {
     function testTemporaryApprove(uint256 actorIndex, address spender, uint256 amount, bool boundSpender, bool boundAmount) external returns (bool) {
@@ -16,11 +20,16 @@ contract FUApprovalsTest is FUDeploy, Test {
             amount = bound(amount, 0, type(uint256).max);
         }
         console.log("amount", amount);
+        console.log("spender", spender);
 
         if (boundSpender) {
             spender = getActor(spender);
         } else {
             maybeCreateActor(spender);
+        }
+
+        if (spender == pair) {
+            expectRevert("need to add require statement to FU.sol to fix");
         }
 
         uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8)))))); //allowance mapping offset is 8, see https://github.com/duncancmt/fu/blob/c64c7b7fbafd1ea362c056e4fecef44ed4ac5688/src/FUStorage.sol#L16-L26
@@ -40,6 +49,25 @@ contract FUApprovalsTest is FUDeploy, Test {
             assertEq(beforeAllowance, afterAllowance, "permit2 allowance should already be maximum");
         }
     }
+
+    /*
+    //TODO: test transferFrom, burnFrom and deliverFrom
+    function testTransferFrom(address actorIndex, address to, uint256 amount, bool boundto, bool boundAmount) external returns (bool) {
+        address actor = getActor(actorIndex);
+        if (boundAmount) {
+            amount = bound(amount, 0, fu.balanceOf(actor), "amount");
+        } else {
+            console.log("amount", amount);
+        }
+        if (actor == pair) {
+            assume(amount < fu.balanceOf(actor));
+        }
+        if (boundTo) {
+            to = getActor(to);
+        } else {
+            maybeCreateActor(to);
+        }
+    } */
 
     // Solidity inheritance is dumb
     function deal(address who, uint256 value) internal virtual override(Common, StdCheats) {
