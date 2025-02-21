e100b7ddae0791fb5c253fa0db99b869e31d3e3a
fluffypill0w <77161947+fluffypill0w@users.noreply.github.com>
2025-02-21 12:58:24 +0100

    Fleshing out approvals tests a bit

--- test/FUApprovals.t.sol
+++ test/FUApprovals.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import {FUDeploy, Common} from "./Deploy.t.sol";
 import {ERC20Base} from "../src/core/ERC20Base.sol";
 import {Context} from "../src/utils/Context.sol";
+import {Settings} from "../src/core/Settings.sol";
 
 import {Test} from "@forge-std/Test.sol";
 import {VmSafe} from "@forge-std/Vm.sol";
@@ -19,6 +20,15 @@ contract FUApprovalsTest is FUDeploy, Test, Context {
         super.setUp();
         actors.push(pair);
         isActor[pair] = true;
+    }
+
+    function _transferFromShouldFail(address from, address to, uint256 amount, uint256 balance)
+        internal
+        view
+        returns (bool)
+    {
+        return from == DEAD || to == DEAD || to == address(fu) || to == from || amount > balance
+            || uint160(to) / Settings.ADDRESS_DIVISOR == 0;
     }
 
     function testTemporaryApprove(uint256 actorIndex, address spender, uint256 amount, bool boundSpender, bool boundAmount) external returns (bool) {
@@ -72,17 +82,29 @@ contract FUApprovalsTest is FUDeploy, Test, Context {
         }
 
         uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+        uint256 beforeBalance = fu.balanceOf(actor);
+        uint256 beforeBalanceTo = fu.balanceOf(to);
 
         vm.recordLogs();
         vm.startStateDiffRecording();
         prank(actor);
 
         (bool success,) = callOptionalReturn(abi.encodeCall(ERC20Base.transferFrom, (actor, to, amount)));
+        assertEq(success, !_transferFromShouldFail(actor, to, amount, beforeBalance), "unexpected failure");
 
         VmSafe.AccountAccess[] memory accountAccesses = vm.stopAndReturnStateDiff();
         VmSafe.Log[] memory logs = vm.getRecordedLogs();
+        //TODO: stuff with these logs
+
+        saveActor(actor);
+        saveActor(to);
 
         uint256 afterAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+        uint256 afterBalance = fu.balanceOf(actor);
+        uint256 afterBalanceTo = fu.balanceOf(to);
+
+        assertEq((beforeBalance - afterBalance), (beforeAllowance - afterAllowance), "change in balances and allowances don't match");
+        assertEq((beforeBalance - afterBalance), (afterBalanceTo - beforeBalanceTo), "changes in `from` and `to` balances don't match");
     }
 
     function testBurnFrom(address actorIndex, uint256 amount, bool boundAmount) external returns (bool) {
@@ -99,17 +121,22 @@ contract FUApprovalsTest is FUDeploy, Test, Context {
         }
 
         uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+        console.log("beforeAllowance:", beforeAllowance);
+        uint256 beforeBalance = fu.balanceOf(actor);
 
         vm.recordLogs();
         vm.startStateDiffRecording();
         prank(actor);
 
-        (bool success,) = callOptionalReturn(abi.encodeCall(ERC20Base.burnFrom, (actor, amount)));
+        (bool success, bytes memory returndata) = callOptionalReturn(abi.encodeCall(ERC20Base.burnFrom, (actor, amount)));
 
         VmSafe.AccountAccess[] memory accountAccesses = vm.stopAndReturnStateDiff();
         VmSafe.Log[] memory logs = vm.getRecordedLogs();
 
         uint256 afterAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+        uint256 afterBalance = fu.balanceOf(actor);
+
+        assertEq((beforeBalance - afterBalance), (beforeAllowance - afterAllowance), "change in balances and allowances don't match");
     }
 
         function testDeliverFrom(address actorIndex, uint256 amount, bool boundAmount) external returns (bool) {
@@ -126,6 +153,7 @@ contract FUApprovalsTest is FUDeploy, Test, Context {
         }
 
         uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+        uint256 beforeBalance = fu.balanceOf(actor);
 
         vm.recordLogs();
         vm.startStateDiffRecording();
@@ -137,6 +165,9 @@ contract FUApprovalsTest is FUDeploy, Test, Context {
         VmSafe.Log[] memory logs = vm.getRecordedLogs();
 
         uint256 afterAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+        uint256 afterBalance = fu.balanceOf(actor);
+
+        assertEq((beforeBalance - afterBalance), (beforeAllowance - afterAllowance), "change in balances and allowances don't match");
     }
 
     // Solidity inheritance is dumb
