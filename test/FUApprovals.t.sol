02aa3b7754823e7b8028287f989296709ea05a5e
fluffypill0w <77161947+fluffypill0w@users.noreply.github.com>
2025-02-21 12:06:43 +0100

    skeleton done for approvals tests

--- test/FUApprovals.t.sol
+++ test/FUApprovals.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.28;
 
 import {FUDeploy, Common} from "./Deploy.t.sol";
 import {ERC20Base} from "../src/core/ERC20Base.sol";
+import {Context} from "../src/utils/Context.sol";
 
 import {Test} from "@forge-std/Test.sol";
 import {VmSafe} from "@forge-std/Vm.sol";
@@ -13,7 +14,13 @@ import {console} from "@forge-std/console.sol";
 address constant PERMIT2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
 address constant pair = 0x00000000e2Ed5120a2fAc7532764DC11F5Ee8fDd;
 
-contract FUApprovalsTest is FUDeploy, Test {
+contract FUApprovalsTest is FUDeploy, Test, Context {
+    function setUp() public override {
+        super.setUp();
+        actors.push(pair);
+        isActor[pair] = true;
+    }
+
     function testTemporaryApprove(uint256 actorIndex, address spender, uint256 amount, bool boundSpender, bool boundAmount) external returns (bool) {
         address actor = getActor(actorIndex);
         if (boundAmount) {
@@ -26,10 +33,6 @@ contract FUApprovalsTest is FUDeploy, Test {
             spender = getActor(spender);
         } else {
             maybeCreateActor(spender);
-        }
-
-        if (spender == pair) {
-            expectRevert("need to add require statement to FU.sol to fix");
         }
 
         uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8)))))); //allowance mapping offset is 8, see https://github.com/duncancmt/fu/blob/c64c7b7fbafd1ea362c056e4fecef44ed4ac5688/src/FUStorage.sol#L16-L26
@@ -50,12 +53,12 @@ contract FUApprovalsTest is FUDeploy, Test {
         }
     }
 
-    /*
-    //TODO: test transferFrom, burnFrom and deliverFrom
-    function testTransferFrom(address actorIndex, address to, uint256 amount, bool boundto, bool boundAmount) external returns (bool) {
+    function testTransferFrom(address actorIndex, address to, uint256 amount, bool boundTo, bool boundAmount) external returns (bool) {
         address actor = getActor(actorIndex);
+        address spender = _msgSender();
+
         if (boundAmount) {
-            amount = bound(amount, 0, fu.balanceOf(actor), "amount");
+            amount = bound(amount, 0, fu.balanceOf(actor));
         } else {
             console.log("amount", amount);
         }
@@ -67,7 +70,74 @@ contract FUApprovalsTest is FUDeploy, Test {
         } else {
             maybeCreateActor(to);
         }
-    } */
+
+        uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+
+        vm.recordLogs();
+        vm.startStateDiffRecording();
+        prank(actor);
+
+        (bool success,) = callOptionalReturn(abi.encodeCall(ERC20Base.transferFrom, (actor, to, amount)));
+
+        VmSafe.AccountAccess[] memory accountAccesses = vm.stopAndReturnStateDiff();
+        VmSafe.Log[] memory logs = vm.getRecordedLogs();
+
+        uint256 afterAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+    }
+
+    function testBurnFrom(address actorIndex, uint256 amount, bool boundAmount) external returns (bool) {
+        address actor = getActor(actorIndex);
+        address spender = _msgSender();
+
+        if (boundAmount) {
+            amount = bound(amount, 0, fu.balanceOf(actor));
+        } else {
+            console.log("amount", amount);
+        }
+        if (actor == pair) {
+            assume(amount < fu.balanceOf(actor));
+        }
+
+        uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+
+        vm.recordLogs();
+        vm.startStateDiffRecording();
+        prank(actor);
+
+        (bool success,) = callOptionalReturn(abi.encodeCall(ERC20Base.burnFrom, (actor, amount)));
+
+        VmSafe.AccountAccess[] memory accountAccesses = vm.stopAndReturnStateDiff();
+        VmSafe.Log[] memory logs = vm.getRecordedLogs();
+
+        uint256 afterAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+    }
+
+        function testDeliverFrom(address actorIndex, uint256 amount, bool boundAmount) external returns (bool) {
+        address actor = getActor(actorIndex);
+        address spender = _msgSender();
+
+        if (boundAmount) {
+            amount = bound(amount, 0, fu.balanceOf(actor));
+        } else {
+            console.log("amount", amount);
+        }
+        if (actor == pair) {
+            assume(amount < fu.balanceOf(actor));
+        }
+
+        uint256 beforeAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+
+        vm.recordLogs();
+        vm.startStateDiffRecording();
+        prank(actor);
+
+        (bool success,) = callOptionalReturn(abi.encodeCall(ERC20Base.deliverFrom, (actor, amount)));
+
+        VmSafe.AccountAccess[] memory accountAccesses = vm.stopAndReturnStateDiff();
+        VmSafe.Log[] memory logs = vm.getRecordedLogs();
+
+        uint256 afterAllowance = uint256(vm.load(address(fu), keccak256(abi.encode(spender, keccak256(abi.encode(actor, uint256(BASE_SLOT) + 8))))));
+    }
 
     // Solidity inheritance is dumb
     function deal(address who, uint256 value) internal virtual override(Common, StdCheats) {
